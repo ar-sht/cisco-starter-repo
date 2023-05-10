d2424ef017f2706a1bd0685a9fa62033872391b1
Ari Shtein <ashtein120@gmail.com>
2023-05-10 10:23:51 -0400

    Task 2 complete - Banner & Exhibit classes & styling

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Banner from './Banner'
+import Exhibit from './Exhibit';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner bannerText="Sextant"/>
+      <Exhibit name="Exhibit 1">
+        <p>Hello</p>
+        <p>Hi</p>
+        <p>How are ya?</p>
+      </Exhibit>
+      <Exhibit name="Exhibit 2">
+        <p>Howdy</p>
+        <p>How's it going?</p>
+        <p>Nice to see you.</p>
+      </Exhibit>
+      <Exhibit name="Exhibit 3">
+        <p>Goodbye</p>
+        <p>See you later</p>
+        <p>Til next time</p>
+      </Exhibit>
     </div>
   );
 }
